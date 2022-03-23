8b7dd5185dc6c4d5e23c1a3c96eeea853fb1e465
Brittany Morris <brittanymorris@ng94r5w3f0.home>
2022-03-23 15:38:49 -0400

    safari fix

--- packages/app/src/components/home/Toolkit.tsx
+++ packages/app/src/components/home/Toolkit.tsx
@@ -26,37 +26,37 @@ export const Toolkit = () => {
               icon: <AppsIcon />,
             },
             {
-              url: 'docs',
+              url: 'manage',
               label: 'Tech Insights',
               icon: <img src="https://e7.pngegg.com/pngimages/607/560/png-clipart-logo-grafana-ansible-kubernetes-blood-pressure-love-miscellaneous-thumbnail.png" style={{width: "41px"}}/>
             },
             {
-              url: 'docs',
+              url: 'catalog/default/group?filters%5Bkind%5D=group&filters%5Buser%5D=all',
               label: 'Groups',
               icon: <PeopleIcon />,
             },
             {
-              url: 'hello',
+              url: 'https://grafana.com/auth/sign-in/?plcmt=top-nav&cta=myaccount',
               label: 'Grafana',
               icon: <img src="https://www.clipartmax.com/png/small/450-4503037_grafana-prometheus-grafana-logo.png" style={{width: "41px"}}/>
             },
             {
-              url: 'hello',
+              url: 'https://ui.honeycomb.io/login',
               label: 'Honeycomb',
               icon: <img src="https://www.honeycomb.io/wp-content/uploads/2021/12/android-chrome-512x512-1.png" style={{width: "41px"}}/>
             },
             {
-              url: 'hello',
+              url: 'https://aws.amazon.com/',
               label: 'AWS',
               icon: <img src="https://icon2.cleanpng.com/20190301/yzq/kisspng-clip-art-logo-yellow-brand-line-aws-logo-transparent-amp-png-clipart-free-downlo-5c7940609561c1.1816411215514502086119.jpg" style={{width: "41px"}}/>
             },
             {
-              url: 'hello',
+              url: 'https://cloud.google.com/docs',
               label: 'GCP',
               icon: <img src="https://www.pinpng.com/pngs/m/17-170783_google-cloud-icon-logo-large-192px-color-google.png" style={{width: "41px"}}/>
             },
             {
-              url: 'hello',
+              url: 'https://login.microsoftonline.com/',
               label: 'Azure',
               icon: <img src="https://www.pinclipart.com/picdir/middle/519-5198040_colorlib-template-microsoft-azure-cloud-icon-clipart.png" style={{width: "41px"}}/>
             },
